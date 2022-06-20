5bf863b366a2000acba9421eecea1ff3595b5bfa
Satish Prasad <spd61185@gmail.com>
2022-06-20 17:08:25 +0530

    Lab4 updated with render data from Student to Course

--- lab4/src/course/Course.js
+++ lab4/src/course/Course.js
@@ -5,60 +5,36 @@ import "./Course.css";
 
 class Course extends Component {
     state = {
-        cid: "C-100",
-        cname: "ReactJS",
-        cost: 15000,
-        trainer: "Satish Prasad",
         showCourse: false
     }
     showCourseInfo = () => {
-        console.log("Button is Clicked");
         let courseFlag = this.state.showCourse;
-
-        //Do not mutate state directly
-        //Use setState() to change the State
-        // this.state.showCourse = !courseFlag;
-
         this.setState({
             showCourse: !courseFlag
         });
     }
 
-    render() {
+    render(){
         let mycourseInfo = null;
 
-        if (this.state.showCourse === true) {
-            mycourseInfo = ( <
-                div className = "mycourse" >
-                <
-                p > Cid: {
-                    this.state.cid
-                } < /p> <
-                p > CourseName: {
-                    this.state.cname
-                } < /p> <
-                p > Cost: {
-                    this.state.cost
-                } < /p> <
-                p > Trainer: {
-                    this.state.trainer
-                } < /p> <
-                /div>
+        if(this.state.showCourse ===true){
+            mycourseInfo = (
+                <div className="mycourse">
+                <p>Cid: {this.props.mycid}</p>
+                <p>CourseName: {this.props.mycname}</p>
+                <p>Cost: {this.props.myprice}</p>
+                <p>Trainer: {this.props.mytrainer}</p>
+             </div>
             )
         }
 
         console.log("I am course");
-        return ( <
-            div >
-            <
-            button className = "mybutton"
-            onClick = {
-                this.showCourseInfo
-            } > ShowCourseInfo < /button> {
-                mycourseInfo
-            } <
-            /div>
-
+        return(
+            <div>
+            <button className="mybutton" onClick={this.showCourseInfo}>ShowCourseInfo</button>
+                    {mycourseInfo}
+            </div>
+         
         )
     }
 }
--- lab4/src/student/Student.js
+++ lab4/src/student/Student.js
@@ -11,55 +11,31 @@ class Student extends Component {
         email: "manish@gmail",
         phone: 123445,
         city: "Bangalore",
-        showStudent: false
-    }
-
-    showStudentInfo = () => {
-        let showStudentFlag = this.state.showStudent;
-        this.setState({
-            showStudent: !showStudentFlag
-        })
-    }
-
-    render() {
-        let myStudentInfo = null;
-        if (this.state.showStudent === true) {
-            myStudentInfo = ( <
-                div className = "mystudent" >
-                <
-                p > Student Details < /p> <
-                p > Sid: {
-                    this.state.sid
-                } < /p> <
-                p > Sname: {
-                    this.state.sname
-                } < /p> <
-                p > Email: {
-                    this.state.email
-                } < /p> <
-                p > Phone: {
-                    this.state.phone
-                } < /p> <
-                p > city: {
-                    this.state.city
-                } < /p> <
-                Course / >
-                <
-                /div>
-            )
+        Course:{
+            cid:"c-1002",
+            couseName:"ReactJS",
+            price:15000,
+            trainer:"Satish"
         }
-
+    }
+    render(){
+    
         console.log("I am Student");
-        return ( <
-            div >
-            <
-            button className = "studentButton"
-            onClick = {
-                this.showStudentInfo
-            } > ShowStudentInfo < /button> {
-                myStudentInfo
-            } <
-            /div>
+        return(
+
+           <div className="mystudent">
+           <p>Student Details</p>
+          <p>Sid: {this.state.sid}</p>
+          <p>Sname: {this.state.sname}</p>
+          <p>Email: {this.state.email}</p>
+          <p>Phone: {this.state.phone}</p>
+          <p>city: {this.state.city}</p>
+           <Course mycid={this.state.Course.cid}
+                    mycname = {this.state.Course.couseName}
+                    myprice = {this.state.Course.price}
+                    mytrainer = {this.state.Course.trainer}
+           />
+           </div>
         );
     }
 }
