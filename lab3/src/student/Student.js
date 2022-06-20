6b653c852ed01b7b156473817df5965759e96f2c
Satish Prasad <spd61185@gmail.com>
2022-06-20 16:24:02 +0530

    Lab3 Student Component updated

--- lab3/src/student/Student.js
+++ lab3/src/student/Student.js
@@ -1,27 +1,65 @@
-import { Component } from 'react';
+import {
+    Component
+} from 'react';
 import Course from '../course/Course';
 import "./Student.css";
 
-class Student extends Component{
-    state ={
-        sid:101,
-        sname:"Mahish",
-        email:"manish@gmail",
-        phone:123445,
-        city:"Bangalore"
+class Student extends Component {
+    state = {
+        sid: 101,
+        sname: "Mahish",
+        email: "manish@gmail",
+        phone: 123445,
+        city: "Bangalore",
+        showStudent: false
     }
-    render(){
+
+    showStudentInfo = () => {
+        let showStudentFlag = this.state.showStudent;
+        this.setState({
+            showStudent: !showStudentFlag
+        })
+    }
+
+    render() {
+        let myStudentInfo = null;
+        if (this.state.showStudent === true) {
+            myStudentInfo = ( <
+                div className = "mystudent" >
+                <
+                p > Student Details < /p> <
+                p > Sid: {
+                    this.state.sid
+                } < /p> <
+                p > Sname: {
+                    this.state.sname
+                } < /p> <
+                p > Email: {
+                    this.state.email
+                } < /p> <
+                p > Phone: {
+                    this.state.phone
+                } < /p> <
+                p > city: {
+                    this.state.city
+                } < /p> <
+                Course / >
+                <
+                /div>
+            )
+        }
+
         console.log("I am Student");
-        return(
-            <div className="mystudent">
-            <p>Student Details</p>
-           <p>Sid: {this.state.sid}</p>
-           <p>Sname: {this.state.sname}</p>
-           <p>Email: {this.state.email}</p>
-           <p>Phone: {this.state.phone}</p>
-           <p>city: {this.state.city}</p>
-            <Course/>
-            </div>
+        return ( <
+            div >
+            <
+            button className = "studentButton"
+            onClick = {
+                this.showStudentInfo
+            } > ShowStudentInfo < /button> {
+                myStudentInfo
+            } <
+            /div>
         );
     }
 }
