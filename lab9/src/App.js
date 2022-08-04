448f96855995ea8a12be06cc99372ae4d8b58ca0
satishlabs <spd61185@gmail.com>
2022-08-04 21:58:06 +0530

    Lab9

---
+++ lab9/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <p>Welcome to Satish Labs</p>
+      </div>
+    )
+  }
+}
+
+export default App;
