e10ee8af7c290f19ead526c2feb66a3aaf4f3e42
satishlabs <spd61185@gmail.com>
2022-09-01 15:43:10 +0530

    Lab13 updated

--- lab13/src/App.js
+++ lab13/src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Welcome to SatishLabs!!</h1>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    console.log("App - constructor()");
+
+    this.state = {
+      compnayName:"JAVA"
+    }
+  }
+
+  render(){
+    console.log(1,"App - render()");
+    console.log(2,"App - ",App.displayName); //static variable
+    console.log(3,"App - ",App.defaultProps); //static variable
+    console.log(4,"App - ",this.state); //instance variable
+    console.log(5,"App - ",this.props); //instance variable
+    return (
+      <div className="container">
+       <h1 className="text-center">Welcome to SatishLabs!!</h1>
+       <br/>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
